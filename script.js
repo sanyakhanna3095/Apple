71e5bf96bfe91672d59875f9933f5ff54e878163
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2024-11-01 01:14:00 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById("menu-icon");
+const mobileNavbar = document.getElementById("mobile-navbar");
+
+menuIcon.addEventListener("click", function () {
+  mobileNavbar.classList.toggle("active");
+  if (mobileNavbar.classList.contains("active") && window.innerWidth<1240) {
+    mobileNavbar.style.display = "block";
+  } else {
+    mobileNavbar.style.display = "none";
+  }
+});
+
